7db2c1d6396f8772c366323fd014c20cb44d23cd
leeeydia <leeeydia@gmail.com>
2026-04-01 12:09:04 +0900

    fix: 전화번호 정규식 하이픈 없는 형태로 수정

--- src/main/java/com/orda/backend/domain/user/dto/request/SignupRequest.java
+++ src/main/java/com/orda/backend/domain/user/dto/request/SignupRequest.java
@@ -36,8 +36,8 @@ public class SignupRequest {
 
     @NotBlank(message = "전화번호는 필수입니다")
     @Pattern(
-            regexp = "^01[0-9]-?\\d{3,4}-?\\d{4}$",
-            message = "전화번호 형식이 올바르지 않습니다 (예: 010-1234-5678)"
+            regexp = "^010\\d{8}$",
+            message = "전화번호 형식이 올바르지 않습니다 (예: 01012345678)"
     )
     private String phone;
 
